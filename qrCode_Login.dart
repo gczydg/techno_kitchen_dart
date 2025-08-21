723af644f04c372011ba3d3846d6ea83e3b7b8b0
dglzy <50166672+gczydg@users.noreply.github.com>
2025-08-21 16:56:27 +0800

    Add files via upload

--- qrCode_Login.dart
+++ qrCode_Login.dart
@@ -1,16 +1,28 @@
+import 'dart:convert';
 import 'package:techno_kitchen_dart/techno_kitchen_dart.dart';
 import 'package:timezone/data/latest_10y.dart' as tzdata;
 import 'dart:io'; 
 
 void main() async {
   tzdata.initializeTimeZones();
+
+  final technoKitchen = TechnoKitchen();
+  final client = technoKitchen.client;
   final milliseconds = DateTime.now().millisecondsSinceEpoch;
   final timestampsinmai = milliseconds ~/ 1000;
   print('当前时间戳: $timestampsinmai');
-  final technoKitchen = TechnoKitchen();
-    print('请输入userId:');
+  // === Step 1: Get UserId by QR Code ===
+    print('请输入qrCode:');
     final input = stdin.readLineSync();
-    final userId = int.parse(input!);
+    final qrCode = input!;
+    final qrResponse = await client.qrApi(qrCode);
+    print('QR Login Response:');
+    print(jsonEncode(qrResponse));
+    if (qrResponse['errorID'] != 0) {
+      print('登陆失败，二维码失效，请获取新的二维码');
+      return;
+    }
+    final userId = qrResponse['userID'];
     // === Step 2: preview ===
     final preview = await technoKitchen.preview(userId);
     print(preview);
@@ -19,9 +31,6 @@ void main() async {
     // === Step 3: login ===
     final login = await technoKitchen.login(userId,timestampsinmai);
     print(login);
-    print('ReturnCode=1代表登陆成功，ReturnCode=102代表二维码过期登陆失败');
-    print('');
-    print('');
     // === Step 4: getTicket ===
     print('请输入付费券倍数（3或6）:');
     final ticketidinput = stdin.readLineSync();
